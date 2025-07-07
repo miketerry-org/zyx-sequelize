c063baa62c9d6813460c96d8cd1a2a104017fc9b
Mike Terry <support@miketerry.org>
2025-07-06 20:56:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js: // zyx-mongodb
+
+"use strict";
+
+// load all necessary modules
+const createDB = require("./lib/createDB");
+const closeDB = require("./lib/closeDB");
+const createLog = require("./lib/createLog");
+const closeLog = require("./lib/closeLog");
+const UserModel = require("./lib/userModel");
+
+module.exports = { createDB, closeDB, createLog, closeLog, UserModel };
